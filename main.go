234f642d48ad2009b178c7a9c15897dacf337bfe
Krish Verma <kverma@gmail.com>
2016-06-30 04:44:06 -0700

    Code cleanup

--- main.go
+++ main.go
@@ -4,27 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	//"os"
 )
 
 func main() {
-
-	// Get current working directory to get the file from it
-	/* cwd, err := os.Getwd()
-	if err != nil {
-		fmt.Printf("Error while getting current directory.")
-		return
-	}
-
-	*/
 	// Command line parsing
-	bind := flag.String("bind", ":80", "Bind address")
-	// root_folder := flag.String("root", cwd, "Root folder")
+	bind := flag.String("bind", ":8080", "Bind address")
 
 	flag.Parse()
 
 	fileServer := http.FileServer(http.Dir("."))
 	fmt.Printf("Sharing current directory on %s ...\n", *bind)
 	http.ListenAndServe((*bind), fileServer)
-
 }
